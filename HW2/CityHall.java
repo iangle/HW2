aa4b1ed772d6156e76124102e630e109531169c5
Peter Tucker <ptucker@whitworth.edu>
2019-01-09 10:07:46 -0800

    feedback

--- HW2/CityHall.java
+++ HW2/CityHall.java
@@ -4,6 +4,9 @@
 package HW2.edu.whitworth.spokane;
 
 public class CityHall extends Building {
+	//PT -- First, try ArrayList (it's like C++ vector).
+	//  Second, it'd useful to add and remove occupants.
+	//  (Maybe have occupants be on the Building object, since all buildings likely have occupants?
 	private Police occupants[];
 	
 	/**@param name,address,occupants. the name, address and the occupants of City Hall*/
